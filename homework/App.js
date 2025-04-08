ac22f134cf1f60d285ceed6fbc7f659e8d0f8d60
neondungeon <mytikvs@proton.me>
2025-04-08 17:37:26 -0300

    e

--- homework/App.js
+++ homework/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer }  from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen   from './screens/HomeScreen';
 import SignInScreen from './screens/SignInScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
