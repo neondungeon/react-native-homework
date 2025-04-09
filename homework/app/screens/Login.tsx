b071d6c92203811d62c3ff0f86d58791d25420a0
neondungeon <mytikvs@proton.me>
2025-04-09 15:35:09 -0300

    final attempt

---
+++ homework/app/screens/Login.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { FIREBASE_AUTH } from "../../FirebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { View, Button, StyleSheet, TextInput, ActivityIndicator, KeyboardAvoidingView } from "react-native";
+
+const Login = () => {
+  const [email, setEmail]   = useState('');
+  const [loading, setLoading] = useState(false);
+  const [password, setPassword] = useState('');
+  
+  const auth = FIREBASE_AUTH;
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      alert('Não foi possível entrar na sua conta: ' + error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(response);
+      alert('Usuário criado com êxito! Verifique o seu Inbox');
+    } catch (error) {
+      console.log(error);
+      alert('Não foi possível criar sua conta: ' + error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAvoidingView behavior="padding">
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+        ></TextInput>
+        <TextInput
+          style={styles.input}
+          placeholder="password"
+          autoCapitalize="none"
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
+          value={password}
+        ></TextInput>
+        { loading ? (
+          <ActivityIndicator size="large" color="#0000ff"/>
+        ) : (
+          <>
+            <Button title="Fazer Login" onPress={signIn}></Button>
+            <Button title="Criar Conta" onPress={signUp}></Button>
+          </>
+        )}
+      </KeyboardAvoidingView>
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    flex: 1,
+    justifyContent: 'center'
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: '#fff'
+  }
+});
